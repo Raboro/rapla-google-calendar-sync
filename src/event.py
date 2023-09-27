2a4b36fba55e7a75548ce76fce464ffd45aaf4b0
Raboro <marius.woerfelreal@gmail.com>
2023-09-27 14:15:42 +0200

    added more properties and date parsing

--- src/event.py
+++ src/event.py
@@ -1,7 +1,10 @@
 from dataclasses import dataclass
+from datetime import datetime
 from dotenv import load_dotenv
 
 import os
+import pytz
+
 
 load_dotenv()
 
@@ -13,6 +16,8 @@ class Event:
     color_id: int
     start: str
     end: str
+    location: str
+    recurrence: str
 
     def parse(self) -> dict[str, str | int]:
         return {
@@ -20,11 +25,17 @@ class Event:
             "description": self.description,
             "colorId": self.color_id,
             "start": {
-                "dateTime": self.start,
-                "timeZone": os.getenv("TINE_ZONE")
+                "dateTime": self.__to_rfc3339(self.start),
+                "timeZone": os.getenv("TIME_ZONE")
             },
             "end": {
-                "dateTime": self.end,
-                "timeZone": os.getenv("TINE_ZONE")
-            }
+                "dateTime": self.__to_rfc3339(self.end),
+                "timeZone": os.getenv("TIME_ZONE")
+            },
+            "location": self.location,
+            "recurrence": [self.recurrence]
         }
+
+    def __to_rfc3339(self, time: str) -> str:
+        datetime_obj = datetime.strptime(time, "%Y%m%dT%H%M%SZ")
+        return datetime_obj.replace(tzinfo=pytz.UTC).isoformat().replace("+00:00", "+02:00")
