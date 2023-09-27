f4397b7e26f3d58a132fe68688217e5f20b920c5
Raboro <marius.woerfelreal@gmail.com>
2023-09-27 14:16:33 +0200

    added event constructing out of ics file

--- src/icalparser.py
+++ src/icalparser.py
@@ -1,6 +1,8 @@
 from dotenv import load_dotenv
+from event import Event
 
 import os
+import icalendar
 import urllib3
 
 
@@ -26,3 +28,23 @@ class IcalParser:
    def __save_to_file(self, data: bytes) -> None:
       with open("../icalFile.ics", "wb") as ical_file: 
         ical_file.write(data)
+
+   def construct_events(self) -> list[Event]:
+      calendar = icalendar.Calendar.from_ical(open("../icalFile.ics", "rb").read())
+      events: list[Event] = []
+      for event in calendar.walk("VEVENT"):
+         if (event.get("RRULE") != None):
+            recurrence = event.get("RRULE").to_ical().decode("utf-8")
+         else:
+            recurrence = ""
+         events.append(Event(
+            summary=event.get("SUMMARY").to_ical().decode("utf-8"),
+            description="" if event.get("DESCRIPTION") == None else event.get("DESCRIPTION").to_ical().decode("utf-8"),
+            color_id=6,
+            start=event.get("DTSTART").to_ical().decode("utf-8"),
+            end=event.get("DTEND").to_ical().decode("utf-8"),
+            location=event.get("LOCATION").to_ical().decode("utf-8") ,
+            recurrence=recurrence
+         ))
+      return events
+            
