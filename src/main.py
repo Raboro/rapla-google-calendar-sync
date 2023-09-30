d9bddfaa8905fe8fe44fe73c9abdbbd65e0d3d25
Raboro <marius.woerfelreal@gmail.com>
2023-09-30 15:42:08 +0200

    added progress bar

--- src/main.py
+++ src/main.py
@@ -1,3 +1,4 @@
+from alive_progress import alive_bar
 from calendarinteractor import CalendarInteractor
 from eventDTO import EventDTO
 from icalparser import IcalParser
@@ -12,12 +13,15 @@ if __name__ == "__main__":
     credentials = calendar_interactor.fetch_credentials()
     service = calendar_interactor.build_service(credentials)
     calendar_id = calendar_interactor.get_calendar_id(service, os.getenv("CALENDAR_NAME"))
-    events: list[EventDTO] = calendar_interactor.fetch_all_events(service, calendar_id)
+    fetched_events: list[EventDTO] = calendar_interactor.fetch_all_events(service, calendar_id)
     counter = 0
-    for event in ical_parser.construct_events():
-        if event.should_be_inserted(events):
-            calendar_interactor.insert_event(event, service, calendar_id)
-            print(event, "was added")
-            counter += 1
+    events = ical_parser.construct_events()
+
+    with alive_bar(len(events)) as bar:
+        for event in events:
+            if event.should_be_inserted(fetched_events):
+                calendar_interactor.insert_event(event, service, calendar_id)
+                counter += 1
+            bar()
     print("\nAdded in total", counter)
     
