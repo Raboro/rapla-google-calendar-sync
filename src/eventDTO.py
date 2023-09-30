42307cd8ea3d06b0432946f26c0bae9ac14bf3c6
Raboro <marius.woerfelreal@gmail.com>
2023-09-30 16:22:20 +0200

    support full day events

--- src/eventDTO.py
+++ src/eventDTO.py
@@ -17,12 +17,26 @@ class EventDTO:
 
     @classmethod
     def from_dict(cls, data: dict):
+        if cls.__is_full_day(data):
+            start = Time(date_time=data["start"]["date"], time_zone="")
+            end = Time(date_time=data["end"]["date"], time_zone="")
+        else:
+            start = Time(date_time=data["start"]["dateTime"], time_zone=data["start"]["timeZone"])
+            end = Time(date_time=data["end"]["dateTime"], time_zone=data["end"]["timeZone"])
+
         return cls(
             summary=data.get("summary", ""),
-            start=Time(date_time=data["start"]["dateTime"], time_zone=data["start"]["timeZone"]),
-            end=Time(date_time=data["end"]["dateTime"], time_zone=data["end"]["timeZone"]),
+            start=start,
+            end=end,
             recurrence=data.get("recurrence", []),
         )
     
+    @classmethod
+    def __is_full_day(cls, data: dict) -> bool:
+        try:
+            return data["start"]["date"] != None
+        except KeyError:
+            return False
+
     def __repr__(self) -> str:
         return f"summary={self.summary} \nstart=[{self.start}] \nend=[{self.end}] \nrecurrence={self.recurrence}"
